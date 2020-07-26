54aa17cd64af75294ece3974e1637aa634ba60d3
bryandms <bryandms95@gmail.com>
2020-07-26 09:32:12 -0600

    Cors added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ export const createApp = () => {
 
   app.use(express.json());
 
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+  });
+
   app.use("/api/auth", auth);
 
   app.use("/api", beer);
