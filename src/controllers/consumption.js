0ba4a0627d8e48e4f84f79d03de9db48697dc7c0
bryandms <bryandms95@gmail.com>
2020-07-27 09:24:26 -0600

    Total consumed endpoint added

--- src/controllers/consumption.js
+++ src/controllers/consumption.js
@@ -12,8 +12,6 @@ export const createConsumption = (req, res, next) => {
         throw error;
       }
 
-      let user = await User.findById(req.userId);
-
       let consumption = new Consumption({
         quantity,
         user: req.userId,
@@ -40,6 +38,36 @@ export const createConsumption = (req, res, next) => {
         message: "Consumption created successfully.",
         consumption: result,
       });
+    })
+    .catch((err) => {
+      if (!err.statusCode) err.statusCode = 500;
+
+      next(err);
+    });
+};
+
+export const getTotalConsumed = (req, res, next) => {
+  User.findById(req.userId)
+    .populate("consumptions")
+    .then((user) => {
+      if (!user) {
+        const error = new Error("Could not find user.");
+        error.statusCode = 404;
+
+        throw error;
+      }
+
+      const getTotalConsumed = (total, consumption) => {
+        return (
+          total + (consumption.user == req.userId ? consumption.quantity : 0)
+        );
+      };
+
+      let totalConsumed = user.consumptions.reduce(getTotalConsumed, 0);
+
+      res
+        .status(200)
+        .json({ message: "Total consumed fetched.", totalConsumed });
     })
     .catch((err) => {
       if (!err.statusCode) err.statusCode = 500;
--- src/routes/consumption.js
+++ src/routes/consumption.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { isAuth } from "../middleware";
-import { createConsumption } from "../controllers";
+import { createConsumption, getTotalConsumed } from "../controllers";
 
 const router = Router();
 
 // POST /api/consumptions
 router.post("/consumptions", isAuth, createConsumption);
+
+// GET /api/totalConsumed
+router.get("/totalConsumed", isAuth, getTotalConsumed);
 
 export { router as consumption };
